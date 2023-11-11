43d3ffdfead695675b9e345be90dc957785eeb9c
Logan Bunelle <lbunelle@bess-f3r5s5.clusters.42paris.fr>
2023-11-11 16:04:52 +0100

    Use '-a' to include all files and '-d xxx.h' to include specific headers

--- source/runner.cpp
+++ source/runner.cpp
@@ -5,12 +5,12 @@
 
 namespace fs = std::filesystem;
 
-int runTest(const std::string &testName, const std::vector<fs::path> &testFiles)
+int runTest(const std::string &testName, const std::vector<fs::path> &testFiles, const std::string &headerName = "", bool includeAll = false)
 {
     int exitCode = 0;
     for (const auto &testFile : testFiles)
     {
-        std::string command = "mkdir -p traces && mkdir -p traces/" + testName + " && gcc -o test_executable " + testName + ".c " + testFile.string() + " -lbsd && ./test_executable > traces/" + testName + "/" + testFile.filename().string() + "_trace.txt";
+        std::string command = "mkdir -p traces && mkdir -p traces/" + testName + " && gcc -o test_executable " + (includeAll ? "*" : testName) + ".c " + testFile.string() + " -lbsd && ./test_executable > traces/" + testName + "/" + testFile.filename().string() + "_trace.txt" + (headerName != "" ? " -DHEADER_NAME=" + headerName : "");
         std::cout << "Running " << testFile.filename().string() << "... ";
         if (std::system(command.c_str()) == 0)
         {
@@ -26,14 +26,18 @@ int runTest(const std::string &testName, const std::vector<fs::path> &testFiles)
     return exitCode;
 }
 
+
 int main(int argc, char **argv)
 {
     std::vector<std::string> functionsToTest;
+    bool includeAll = false;
+    bool includeHeader = false;
+    std::string headerName = "";
 
     std::cout << "\033[34m----------------------------------------\n";
     std::cout << "| \033[36m42 Tests Runner\033[34m                      |\n";
     std::cout << "| \033[36mBy: @lbunelle\033[34m                        |\n";
-    std::cout << "----------------------------------------\033[0m\n\n";
+    std::cout << "----------------------------------------\033[0m\n";
     if (argc == 1)
     {
         for (const auto &entry : fs::directory_iterator("tests"))
@@ -48,8 +52,35 @@ int main(int argc, char **argv)
     {
         for (int i = 1; i < argc; ++i)
         {
-            functionsToTest.push_back(argv[i]);
+            if (argv[i][0] == '-')
+            {
+                if (argv[i][1] == 'd')
+                {
+                    includeHeader = true;
+                    if (i + 1 >= argc)
+                    {
+                        std::cout << "\033[31mNo header name provided.\033[0m\n";
+                        return 1;
+                    }
+                    headerName = argv[i + 1];
+                    i++;
+                    std::cout << "\033[33mIncluding header " << headerName << ".\033[0m\n";
+                }
+                else if (argv[i][1] == 'a')
+                {
+                    includeAll = true;
+                    std::cout << "\033[33mIncluding all files.\033[0m\n";
+                }
+                else
+                {
+                    std::cout << "\033[31mInvalid argument " << argv[i] << ".\033[0m\n";
+                    return 1;
+                }
+            }
+            else
+                functionsToTest.push_back(argv[i]);
         }
+        std::cout << "\n";
     }
 
     for (const auto &functionName : functionsToTest)
@@ -68,7 +99,7 @@ int main(int argc, char **argv)
             testFiles.push_back(testFile.path());
         }
 
-        int code = runTest(functionName, testFiles);
+        int code = runTest(functionName, testFiles, headerName, includeAll);
         if(code == 0)
         {
             std::cout << "\033[32mAll tests passed for " << functionName << "\033[0m\n\n";
